a9134f40333afbff30bb881abdc6d7a9eda36f8a
longxianghui <longxianghui@fulu.com>
2019-10-22 12:46:04 +0800

    添加项目文件。

---
+++ Grpc.Client/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Grpc.Core;
+using Grpc.Net.Client;
+using Grpc.Server;
+using IdentityModel.Client;
+
+namespace Grpc.Client
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            //获取token可以直接使用HttpClient来获取，这里使用IdentityModel来获取token
+            var httpClient = new HttpClient();
+            var disco = await httpClient.GetDiscoveryDocumentAsync("http://localhost:5000");
+            if (!disco.IsError)
+            {
+                var token = await httpClient.RequestClientCredentialsTokenAsync(new ClientCredentialsTokenRequest()
+                {
+                    Address = disco.TokenEndpoint,
+                    ClientId = "client",
+                    ClientSecret = "secret"
+                });
+                var tokenValue = "Bearer " + token.AccessToken;
+                var metadata = new Metadata
+                {
+                    { "Authorization", tokenValue }
+                };
+                var callOptions = new CallOptions(metadata);
+                var channel = GrpcChannel.ForAddress("https://localhost:5001");
+                var client = new Greeter.GreeterClient(channel);
+                var response = client.SayHello(new HelloRequest { Name = "World" }, callOptions);
+                Console.WriteLine(response.Message);
+            }
+        }
+    }
+}
---
+++ Ids4.Server/Config.cs
@@ -0,0 +1,45 @@
+﻿using IdentityServer4.Models;
+using System.Collections.Generic;
+using System.Security.Claims;
+using IdentityModel;
+using IdentityServer4.Test;
+
+namespace Ids4.Server
+{
+    public class Config
+    {
+        public static IEnumerable<IdentityResource> GetIdentityResources()
+        {
+            return new List<IdentityResource>
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile(),
+                new IdentityResources.Email(),
+            };
+        }
+        public static IEnumerable<ApiResource> GetApis()
+        {
+            return new List<ApiResource>
+            {
+                new ApiResource("api", "Demo API")
+                {
+                    ApiSecrets = { new Secret("secret".Sha256()) }
+                }
+            };
+        }
+        public static IEnumerable<Client> GetClients()
+        {
+            return new List<Client>
+                {
+                    new Client
+                    {
+                        ClientId = "client",
+                        ClientSecrets = { new Secret("secret".Sha256()) },
+
+                        AllowedGrantTypes = GrantTypes.ClientCredentials,
+                        AllowedScopes = { "api" },
+                    },
+                };
+        }
+    }
+}
